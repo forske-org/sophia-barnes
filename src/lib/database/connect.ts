3a74ca56c5afabb1f9422a405a07d8130cf6451f
Manuel Nielsen <MaNielsen@forske.org>
2025-09-27 17:02:48 +1000

    FIX: rsa key newline fix

--- src/lib/database/connect.ts
+++ src/lib/database/connect.ts
@@ -15,20 +15,13 @@
 // )
 
 import snowflake from 'snowflake-sdk'
-import crypto from 'crypto'
 
 export const db = snowflake.createPool({
     account: process.env.SNOWFLAKE_ACCOUNT!,
     warehouse: process.env.SNOWFLAKE_WAREHOUSE!,
     username: process.env.SNOWFLAKE_USERNAME!,
     // password: process.env.SNOWFLAKE_PASSWORD!,
-    privateKey: crypto.createPrivateKey({
-        key: process.env.SNOWFLAKE_PRIVATE_KEY!.replaceAll('\\n', '\n'),
-        format: 'pem',
-    }).export({
-        format: 'pem',
-        type: 'pkcs8',
-    }).toString(),
+    privateKey: `-----BEGIN PRIVATE KEY-----\n${process.env.SNOWFLAKE_PRIVATE_KEY!}\n-----END PRIVATE KEY-----`,
     authenticator: "SNOWFLAKE_JWT",
     role: process.env.SNOWFLAKE_ROLE!,
     database: process.env.SNOWFLAKE_DATABASE!,
